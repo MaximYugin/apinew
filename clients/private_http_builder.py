3bc14df1e8e562d57ca58ef299aea22317bc2ea7
MaximYugin <102412402+MaximYugin@users.noreply.github.com>
2026-08-14 17:07:11 +0300

    e

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from typing import TypedDict
 
 from httpx import Client
@@ -7,12 +8,13 @@ from clients.authentication.authentication_client import get_authentication_clie
 from clients.authentication.authentication_schema import LoginRequestSchema
 
 
-class AuthenticationUserSchema(BaseModel):  # Структура данных пользователя для авторизации
+class AuthenticationUserSchema(BaseModel, frozen=True):  # Структура данных пользователя для авторизации
     email: str
     password: str
 
 
 # Создаем private builder
+@lru_cache(maxsize=None)  # Кешируем возвращаемое значение
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     """
     Функция создаёт экземпляр httpx.Client с аутентификацией пользователя.
